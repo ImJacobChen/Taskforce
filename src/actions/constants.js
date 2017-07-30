00c906751f145fa7c867d0b4bf5ec9018fa768d6
JacobChen1994 <jacobsenpai@outlook.com>
2017-07-30 17:38:24 +0100

    Add RECEIVE_TASK constant

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -4,4 +4,6 @@ export const GET_TASKS = 'TASKS:GET_TASKS';
 
 export const ADD_TASK = 'TASKS:ADD_TASK';
 
+export const RECEIVE_TASK = 'TASKS:RECEIVE_TASK';
+
 export const DELETE_TASK = 'TASKS:DELETE_TASK';
