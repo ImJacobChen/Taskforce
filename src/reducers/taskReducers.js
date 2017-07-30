fe0a9095d0879f2e5124275d713b4f6563d8bfdd
JacobChen1994 <jacobsenpai@outlook.com>
2017-07-30 14:08:38 +0100

    Add reducer action handlers to taskReducers

--- src/reducers/taskReducers.js
+++ src/reducers/taskReducers.js
@@ -2,7 +2,27 @@
 
 export default taskReducers(state={tasks:[]}, action) {
     switch(action.type) {
-        
+        case 'GET_TASKS':
+            return {...state, tasks:[...state.tasks]};
+            break;
+
+        case 'ADD_TASK': 
+            let tasks = state.tasks.concat(action.payload);
+            return state = {tasks};
+            break;
+
+        case 'DELETE_TASK':
+            const tasksToDelete = state.tasks.slice(0);
+
+            const indexToDelete = tasksToDelete.findIndex(
+                function(task) {
+                    return task.id === action.payload.id;
+                }
+            );
+
+            let newTasks = tasksToDelete.splice(indexToDelete, 1);
+            return state = {tasks: newTasks};
+            break;
     }
 
     return state;
