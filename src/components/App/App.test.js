b76532162c819177da84167e3a3c62ccb4aa3eeb
JacobChen1994 <jacobsenpai@outlook.com>
2017-08-19 12:09:34 +0100

    - Install --save-dev 'react-test-renderer' - Test <App />

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App />', () => {
+
+  beforeAll(() => {
+
+  });
+
+  afterAll(() => {
+  
+  });
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+
+  it('calls openCreateTaskModal() on "Create task" click', () => {
+    let openCreateTaskModalMock = jest.fn()
+    App.prototype.openCreateTaskModal = openCreateTaskModalMock;
+
+    const wrapper = shallow(<App />);
+    wrapper.setState({ user: true, isCreateTaskModalOpen: false, });
+
+    wrapper.find('.App-header-createTaskButton').simulate('click');
+    expect(openCreateTaskModalMock.mock.calls.length).toBe(1);
+  });
+
+  it('calls signOut() on \'Sign out\' click', () => {
+    let signOutMock = jest.fn();
+    App.prototype.signOut = signOutMock;
+
+    const wrapper = shallow(<App />);
+    wrapper.setState({ user: true, isCreateTaskModalOpen: false });
+
+    wrapper.find('.App-header-signOutButton').simulate('click');
+    expect(signOutMock.mock.calls.length).toBe(1);
+  });
+
 });
+
